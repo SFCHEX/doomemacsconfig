cbb5858e0be06830ba0afce98813391570c51e7a
CHEX <52354962+SFCHEX@users.noreply.github.com>
2021-05-28 11:05:30 +0200

    Add files via upload

---
+++ custom.el
@@ -0,0 +1,45 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#282a36" "#ff5555" "#50fa7b" "#f1fa8c" "#61bfff" "#ff79c6" "#8be9fd" "#f8f8f2"])
+ '(custom-safe-themes
+   '("f471bd40dabcce38d14fcbc2779e3974c3f74df0515b3f6785df4dfbd678566a" "6ef24380d82b651d2c0a3974429e2dcd9dde4282f77b798a790f4c3cd864b813" "cebda623e3f9ecd4a4af08d92c406bc4517e323d9259d5156f43901860224e30" "e3713032de69725c7dd17688460b81657a086bc1d8c2ccff85ba2c98ce5b4f65" "e3c64e88fec56f86b49dcdc5a831e96782baf14b09397d4057156b17062a8848" "cbe59faa15a5f52bdf110b822509b1bbe7f080997c036d0ceb22675adc2e4d76" "e3b2bad7b781a968692759ad12cb6552bc39d7057762eefaf168dbe604ce3a4b" "293dcc6719f6c1547f266a90b3f92138278265dad55f192c57d8b2d903191112" "c4ec44cce043a30c57dd3a1342a890991976a872cd71f4c0650c9387a66eb3f4" "5b809c3eae60da2af8a8cfba4e9e04b4d608cb49584cb5998f6e4a1c87c057c4" "ff195a1c80d74fdfece0993e5eb763be25163ab8981175899d19c3023dc90e23" "3b7e1eecfb3329a613c8362fb08470e741d477424e506f3f444c42bc13ede829" "e68bd30d8796a211ff2dbe1dc155f7303853547d9c7fd36b019f6bd732890854" "0e2a7e1e632dd38a8e0227d2227cb8849f877dd878afb8219cb6bcdd02068a52" "22c8110ceeade36347426926ae4adac25067743f49354fd04d9d49c051cb5149" "22dadcc3e29840ef054c188b5cfe88c71c3bf77afc58d1c4ff9ab5dd3ef60e9b" "b35a14c7d94c1f411890d45edfb9dc1bd61c5becd5c326790b51df6ebf60f402" "fd86f8746ab30d970d927449b11829e43c286410566b43ea013c8779e2df6d64" "95d0ed21bb0e919be7687a25ad59a1c2c8df78cbe98c9e369d44e65bfd65b167" "63bfcabeb44559c67d8827dc68cd6c4a6d3ce35ef4504343af12d42f24894e00" "6c727d2d8992257506352dae2c684406965dea08ce1135db5ba2c7ea4a9b885c" "34e89196734641234800804ecdb2c20505e81586b816cc906eb319c377225ff5" "f65451261f23f770b06f2ea96a9d349c5c7eaec15cc057873010f2f11a4c7229" "fd52492c2f9424e270f677841998403460f9dcc283e7f035fda304dbbc7f67a3" "2c9dc3ab1cbde9d4a3cd129b945d39e19fe7107708edec589118b14d24c01dac" "6fa878330926f7b021c214be11f15d3dc36b2f5969d8111fde27ba00ac016bbd" "8fd2d543bdaa2858240dd7814bd8ee78c869f90bffed37a06316dd6407510260" "0fe24de6d37ea5a7724c56f0bb01efcbb3fe999a6e461ec1392f3c3b105cc5ac" default))
+ '(fci-rule-color "#6272a4")
+ '(jdee-db-active-breakpoint-face-colors (cons "#1E2029" "#bd93f9"))
+ '(jdee-db-requested-breakpoint-face-colors (cons "#1E2029" "#50fa7b"))
+ '(jdee-db-spec-breakpoint-face-colors (cons "#1E2029" "#565761"))
+ '(objed-cursor-color "#ff5555")
+ '(pdf-view-midnight-colors (cons "#f8f8f2" "#282a36"))
+ '(rustic-ansi-faces
+   ["#282a36" "#ff5555" "#50fa7b" "#f1fa8c" "#61bfff" "#ff79c6" "#8be9fd" "#f8f8f2"])
+ '(vc-annotate-background "#282a36")
+ '(vc-annotate-color-map
+   (list
+    (cons 20 "#50fa7b")
+    (cons 40 "#85fa80")
+    (cons 60 "#bbf986")
+    (cons 80 "#f1fa8c")
+    (cons 100 "#f5e381")
+    (cons 120 "#face76")
+    (cons 140 "#ffb86c")
+    (cons 160 "#ffa38a")
+    (cons 180 "#ff8ea8")
+    (cons 200 "#ff79c6")
+    (cons 220 "#ff6da0")
+    (cons 240 "#ff617a")
+    (cons 260 "#ff5555")
+    (cons 280 "#d45558")
+    (cons 300 "#aa565a")
+    (cons 320 "#80565d")
+    (cons 340 "#6272a4")
+    (cons 360 "#6272a4")))
+ '(vc-annotate-very-old-color nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(vertical-border ((t (:foreground "#383539")))))
